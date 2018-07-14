309cd9db0d48fb999a2d2c07a75b934d6fed6fbb
陈晟 <chensheng@dtinsure.com>
2018-07-14 11:46:55 +0800

    提交代码

--- spring-boot-activiti/src/main/java/com/feiniu/activiti/Mail.java
+++ spring-boot-activiti/src/main/java/com/feiniu/activiti/Mail.java
@@ -7,16 +7,16 @@ public class Mail {
 	public static void main(String[] args) {
 		try {
 			SimpleEmail email = new SimpleEmail();
-			email.setHostName("smtp.qq.com");
-			email.setSmtpPort(587);
-			email.setStartTLSEnabled(true);
+			email.setHostName("smtp.dtinsure.com");
+			email.setSmtpPort(25);
+			email.setStartTLSEnabled(false);
 			email.setSSLOnConnect(false);
 			// 收件人的邮箱
-			email.addTo("251382101@qq.com", "chensheng");
+			email.addTo("chensheng@dtinsure.com", "chensheng");
 			// 发送人的邮箱
-			email.setFrom("251382101@qq.com", "鸡博士");
+			email.setFrom("chensheng@dtinsure.com", "鸡博士");
 			// 邮件认证
-			email.setAuthentication("251382101@qq.com", "xxxxxx");
+			email.setAuthentication("chensheng@dtinsure.com", "123456.com");
 			email.setSubject("Test message");
 			email.setMsg("This is a simple test of commons-email..");
 			email.send();
